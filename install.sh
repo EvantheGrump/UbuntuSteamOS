b1ec5d02ae78ef508a18254fb85677f39e366320
GrumpyEvan <70341827+GrumpyEvan@users.noreply.github.com>
2023-04-15 22:43:12 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ apt update -y && apt upgrade -y
 # install steam and screen output
 apt install steam xorg openbox xinit
 
+# install drivers for steam
+sudo apt install nvidia-driver-libs:i386
+
 # start the screen
 startx
 
